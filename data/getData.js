8e94884cc249f5e3d208a2b8e7c0d9b304f15211
yiyu0x <tako0903@icloud.com>
2019-08-12 12:03:05 +0800

    Add 1081

--- data/getData.js
+++ data/getData.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 var path = require('path');
 const parser = require('xml2json');
 
-const url = 'https://api.ncnu.edu.tw/API/get.aspx?xml=course_ncnu&year=107&semester=2&unitId=all';
+const url = 'https://api.ncnu.edu.tw/API/get.aspx?xml=course_ncnu&year=108&semester=1&unitId=all';
 const file_path = path.join(__dirname, 'course_data.json');
 axios.get(url).then(function(response){
 	// console.log(response.data); // ex.: { user: 'Your User'}
